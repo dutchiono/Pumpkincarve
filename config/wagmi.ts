827be74c91f634a7b3f0ac1bb40c671a245841c6
dutch <dutchiono@gmail.com>
2025-10-25 15:55:55 -0400

    fix error3

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -4,11 +4,6 @@ import { base, baseSepolia } from 'wagmi/chains';
 
 const farcasterConnector = miniAppConnector();
 
-// Monkey-patch getChainId to always return base sepolia chain id
-if (farcasterConnector && typeof farcasterConnector.getChainId === 'undefined') {
-  (farcasterConnector as any).getChainId = async () => baseSepolia.id;
-}
-
 export const config = createConfig({
   chains: [baseSepolia, base],
   transports: {
