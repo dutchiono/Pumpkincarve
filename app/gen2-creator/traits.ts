24fa3804629eff102c016d3a8aa0e7c9273a9d66
dutch <your-dutchiono-email@example.com>
2025-10-28 11:09:05 -0400

    wetood

--- app/gen2-creator/traits.ts
+++
@@ -1,8 +0,0 @@
-export const traits = {
-  Background: ['Blue', 'Green', 'Red', 'Yellow'],
-  Base: ['Pumpkin', 'Tomato', 'Watermelon'],
-  Eyes: ['Angry', 'Happy', 'Sad', 'Wink'],
-  Mouth: ['Smile', 'Frown', 'O-shape', 'Tongue'],
-  Nose: ['Small', 'Large', 'Pointy', 'None'],
-  Accessories: ['Hat', 'Bowtie', 'Glasses', 'None'],
-};
